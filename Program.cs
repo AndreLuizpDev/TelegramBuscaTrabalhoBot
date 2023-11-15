80acade6a5db4d776bb59b6b1b06fc9a8a8fbd30
AndreLuizpDev <andreldsantosp@gmail.com>
2023-11-15 14:49:18 -0300

    Changed polling for webhook and other small adjustments.

--- Program.cs
+++ Program.cs
@@ -5,20 +5,22 @@ using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 class Program
 {
-    private static DatabaseService databaseService;
+    private static string connectionString = ConfigurationManager.AppSettings["TelegramBotConnection"];
+    private static DatabaseService databaseService = new DatabaseService(connectionString);    
+    private static TelegramBotClient botClient = new TelegramBotClient(ConfigurationManager.AppSettings["BotToken"]);
+    private static CancellationTokenSource cts = new CancellationTokenSource();
+    private static ReceiverOptions receiverOptions;
+
     static async Task Main(string[] args)
     {
-        var botClient = new TelegramBotClient(ConfigurationManager.AppSettings["BotToken"]);
-        var connectionString = ConfigurationManager.AppSettings["TelegramBotConnection"];
         var dbConnection = new MySqlConnection(connectionString);
-
         databaseService = new DatabaseService(connectionString);
         databaseService.InitializeDatabase();
-
-        using CancellationTokenSource cts = new();
 
         ReceiverOptions receiverOptions = new()
         {
@@ -45,7 +47,7 @@ class Program
         List<string> commands = new List<string>
     {
         "*/start* - Start the bot",
-        "*/includeFreelancer* <Name> <Experience> <Description> - Add a new user",
+        "*/includeFreelancer* <Name>;<Experience>;<ContatoTelegram>;<ContatoEmail>;<ContatoTelefonico>;<Description> - Add a new user",
         "*/listFreelancers* - List all Freelancers"
     };
 
@@ -53,6 +55,10 @@ class Program
     }
     async static Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
+        var factory = new ConnectionFactory() { Uri = new Uri(ConfigurationManager.AppSettings["CloudAMQPUrl"]) };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
         if (update.Message is not { } message)
             return;
 
@@ -63,36 +69,71 @@ class Program
 
         if (messageText != null)
         {
+
+            List<string> availableCommands = GetAvailableCommands();
+            string response = "Available commands:\n" + string.Join("\n", availableCommands);
+
             if (messageText.StartsWith("/start"))
             {
-                List<string> availableCommands = GetAvailableCommands();
-                string response = "Available commands:\n" + string.Join("\n", availableCommands);
-                await botClient.SendTextMessageAsync(message.Chat.Id, response, parseMode: ParseMode.Markdown);
+                await botClient.SendTextMessageAsync(chatId, "Welcome to our Job Search Bot! 🤖✨\r\nExplore opportunities, add your freelancer profile, and connect with other users. Use commands like \r\n/includeFreelancer or /listFreelancers to get started. 🚀💼\r\n");
             }
             else if (messageText.StartsWith("/includeFreelancer"))
             {
-                var parts = messageText.Split(' ', 4); // Split the message into parts
-                if (parts.Length < 4)
+                var parts = messageText.Split(';', StringSplitOptions.RemoveEmptyEntries); // Split the message into parts
+                if (parts.Length < 6)
                 {
-                    await botClient.SendTextMessageAsync(chatId, "Invalid format. Please use: /createUser <name> <experience> <description>");
+                    await botClient.SendTextMessageAsync(chatId, "Invalid format. Please use: /includeFreelancer <name>;<experience>;<ContatoTelegram>;<ContatoEmail>;<ContatoTelefonico>;<description>");
                     return;
                 }
 
-                var name = parts[1];
-                var experience = int.Parse(parts[2]);
-                var description = parts[3];
+                var name = parts[0].Replace("/includeFreelancer ", "");
+                var experience = int.Parse(parts[1]);
+                var contatoTelegram = parts[2];
+                var contatoEmail = parts[3];
+                var contatoTelefonico = parts[4];
+                var description = parts[5];
 
-                databaseService.CreateUser(name, experience, description);
+                databaseService.CreateUser(chatId, name, experience, contatoTelegram, contatoEmail, contatoTelefonico, description);
                 await botClient.SendTextMessageAsync(chatId, "User created successfully.");
+
+                var body = Encoding.UTF8.GetBytes($"{chatId};{name};{experience};{contatoTelegram};{contatoEmail};{contatoTelefonico};{description}");
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "UserQueue",
+                                     basicProperties: null,
+                                     body: body);
             }
             else if (messageText.StartsWith("/listFreelancers"))
             {
-                string userList = databaseService.GetUsersList();
-                await botClient.SendTextMessageAsync(chatId, userList, parseMode: ParseMode.Markdown);
+
+                List<string> userList = databaseService.GetUsersList();
+
+                // Enviar cada item da lista como uma mensagem separada
+                foreach (var user in userList)
+                {
+                    await botClient.SendTextMessageAsync(chatId, user, parseMode: ParseMode.Markdown);
+                    // Aguarde um curto período de tempo entre as mensagens para evitar limites do Telegram
+                    await Task.Delay(500);
+                }
+            }
+            else
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, response, parseMode: ParseMode.Markdown);
             }
         }
-    }
 
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body;
+            var message = Encoding.UTF8.GetString(body.ToArray());
+            Console.WriteLine("Received from CloudAMQP: {0}", message);
+        };
+        channel.BasicConsume(queue: "UserQueue",
+                             autoAck: true,
+                             consumer: consumer);
+
+        await Task.Delay(1000);    
+    }
     static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
     {
         var errorMessage = exception switch
--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -51,70 +51,91 @@ public class DatabaseService
     private string CreateUsersTable()
     {
         string query = @"
-            CREATE TABLE users (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                user_id INT NOT NULL,
-                name VARCHAR(255) NOT NULL,
-                experience INT,
-                description TEXT
-            )";
-        
-        using (var cmd = new MySqlCommand(query, dbConnection))
+        CREATE TABLE users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id INT NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            experience INT,
+            ContatoTelegram VARCHAR(255),
+            ContatoEmail VARCHAR(255),
+            ContatoTelefonico VARCHAR(20),
+            description TEXT
+        )";
+
+        try
         {
-            cmd.ExecuteNonQuery();
-            operationMessage = "Tabelas criadas com sucesso.";
+            using (var cmd = new MySqlCommand(query, dbConnection))
+            {
+                cmd.ExecuteNonQuery();
+                operationMessage = "Tabelas criadas com sucesso.";
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error creating table: {ex.Message}");
+            operationMessage = "Erro ao criar tabelas.";
         }
         return operationMessage;
     }
-    public string CreateUser(string name, int experience, string description)
+    public void CreateUser(long chatId, string name, int experience, string contatoTelegram, string contatoEmail, string contatoTelefonico, string description)
     {
         try
         {
             dbConnection.Open();
-            string query = "INSERT INTO users (name, experience, description) VALUES (@name, @experience, @description)";
+            string query = "INSERT INTO users (user_id, name, experience, ContatoTelegram, ContatoEmail, ContatoTelefonico, description) VALUES (@user_id, @name, @experience, @contatoTelegram, @contatoEmail, @contatoTelefonico, @description)";
+
             using MySqlCommand cmd = new MySqlCommand(query, dbConnection);
+            cmd.Parameters.AddWithValue("@user_id", chatId);
             cmd.Parameters.AddWithValue("@name", name);
             cmd.Parameters.AddWithValue("@experience", experience);
+            cmd.Parameters.AddWithValue("@contatoTelegram", contatoTelegram);
+            cmd.Parameters.AddWithValue("@contatoEmail", contatoEmail);
+            cmd.Parameters.AddWithValue("@contatoTelefonico", contatoTelefonico);
             cmd.Parameters.AddWithValue("@description", description);
+
             cmd.ExecuteNonQuery();
-            operationMessage = "Usuário cadastrado com sucesso!";
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error creating user: {ex.Message}");
-            operationMessage = "Erro ao criar tabela.";
         }
         finally
         {
             dbConnection.Close();
         }
-        return operationMessage;
     }
-
-    public string GetUsersList()
+    public List<string> GetUsersList()
     {
+        List<string> userList = new List<string>();
+
         try
         {
             dbConnection.Open();
-            string query = "SELECT name, experience, description FROM users";
+            string query = "SELECT name, experience, ContatoTelegram, ContatoEmail, ContatoTelefonico, description FROM users";
             MySqlCommand cmd = new MySqlCommand(query, dbConnection);
+
             using (MySqlDataReader reader = cmd.ExecuteReader())
             {
-                string userList = "*Relação de Freelancers:*\n\n";
                 while (reader.Read())
                 {
-                    userList += $"*Nome: * {reader["name"]}\n" +
-                                $"*Experiência: * {reader["experience"]}\n" +
-                                $"*Descrição: * {reader["description"]}\n\n";
+                    string userEntry = $"*Nome: * {reader["name"]}\n" +
+                                       $"*Experiência: * {reader["experience"]}\n" +
+                                       $"*Contato Telegram: * {reader["ContatoTelegram"]}\n" +
+                                       $"*Contato Email: * {reader["ContatoEmail"]}\n" +
+                                       $"*Contato Telefônico: * {reader["ContatoTelefonico"]}\n" +
+                                       $"*Descrição: * {reader["description"]}\n\n";
 
+                    userList.Add(userEntry);
                 }
-                return userList;
             }
+
+            return userList;
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error retrieving user list: {ex.Message}");
-            return "An error occurred while retrieving the user list.";
+            // Se ocorrer um erro, você pode retornar uma lista vazia ou lidar com isso de outra maneira
+            return new List<string>();
         }
         finally
         {
